b2e97dcd291bb8982958c954644384fec7680b6f
tianhuiting <542211654@qq.com>
2016-04-13 10:21:17 +0800

    task17-1

--- js/task16.js
+++ js/task16.js
@@ -10,13 +10,13 @@ function verify() {
     var result = false;
     var city = document.getElementById('aqi-city-input');
     var value = document.getElementById('aqi-value-input');
-    if (city.value == '' && value.value == '') {
+    if (city.value === '' && value.value === '') {
         alert('请输入内容');
     }
-    else if (city.value == '') {
+    else if (city.value === '') {
         alert('请输入城市名称');
     }
-    else if (value.value == '') {
+    else if (value.value === '') {
         alert('请输入空气质量指数');
     }
     else {
@@ -39,10 +39,10 @@ function addRow() {
     var city = document.getElementById('aqi-city-input');
     var value = document.getElementById('aqi-value-input');
     var tables = document.getElementsByTagName('table')[0];
-    if (i == 0) {
+    if (i === 0) {
         tables.innerHTML = '<tr><td>城市</td><td>空气质量</td><td>操作</td></tr>';
-    };
-    tables.innerHTML += '<tr><td>' + city.value + '</td><td>' + value.value + '</td><td><button>删除</button></td></tr>'
+    }
+    tables.innerHTML += '<tr><td>' + city.value + '</td><td>' + value.value + '</td><td><button>删除</button></td></tr>';
     i++;
     bindDeleteEvent();
 }
@@ -64,7 +64,7 @@ function CheckEmpName(EmpName)
     var CheckTestName = "^[a-zA-Z\u4e00-\u9fa5]+$";
     var re = new RegExp(CheckTestName);
     var result = false;
-    if(EmpName.length!=0)
+    if(EmpName.length!==0)
     {
         if(re.test(EmpName))
         {
